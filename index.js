c7e270c9fbd4a5258ce97d462f884cff3b69b2e8
Mary Lark <mary@marylark.com>
2018-11-15 23:54:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var greeting = "Hello, everybody!";
+var specialGuest = "Neil deGrasse Tyson";
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
+var topic = "";
+var conversation = `Let's talk about ${topic}.`
+
+
+
+
+
