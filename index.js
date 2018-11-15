ef2d034e64e4cdf3c987f12d6c94e6875b3f6574
Mary Lark <mary@marylark.com>
2018-11-15 23:50:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+
+greeting = "Hello, everybody!";
+greetSpecialGuest = `Hello, ${specialGuest}!`;
+conversation = `Let's talk about ${topic}.`;
+/*
+})
+
+it('interpolates a string in `conversation`', function() {
+  expect(conversation).toEqual(`Let's talk about ${topic}.`)
+})
+
+*/
